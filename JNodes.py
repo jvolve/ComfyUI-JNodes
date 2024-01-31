75aa89d217a1d529be12f153fe0df50d79b48217
joelivolve <jhewitt@ivolvestudios.com>
2024-01-31 16:24:41 +1100

    changes to make it actually work

--- JNodes.py
+++ JNodes.py
@@ -1,11 +1,13 @@
 import re
 
+# STRINGS
+
 class J_StringContains:
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "input": ("STRING", { "multiline": False, "default": "", "forceInput": True }),
+                "input_str": ("STRING", { "multiline": False, "default": "", "forceInput": True }),
                 "substring": ("STRING", { "multiline": False, "default": "" }),
                 "success_result": ("STRING", { "multiline": False, "default": "" }),
                 "default_result": ("STRING", { "multiline": False, "default": "" }),
@@ -32,8 +34,8 @@ class J_StringAppend:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "input": ("STRING", { "multiline": False, "default": "", "forceInput": True }),
-                "append": ("STRING", { "multiline": False, "default": "" }),
+                "input_str": ("STRING", { "multiline": False, "default": "", "forceInput": True }),
+                "append_str": ("STRING", { "multiline": False, "default": "" }),
                 "separator": ("STRING", { "multiline": False, "default": "" }),
             },
         }
@@ -57,7 +59,7 @@ class J_StringSplit:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "input": ("STRING", { "multiline": False, "default": "", "forceInput": True }),
+                "input_str": ("STRING", { "multiline": False, "default": "", "forceInput": True }),
                 "split_chars": ("STRING", { "multiline": False, "default": "," }),
                 "trim_whitespace": ("BOOLEAN", { "default": True }),
                 "other_trim_chars": ("STRING", { "multiline": False, "default": "" }),
@@ -88,6 +90,7 @@ class J_StringSplit:
         
         for i, s in enumerate(result):
             result[i] = s.strip(unique_trim_chars)
+            # print(f"J_StringSplit.string_split(): {result[i]}")
         
         return (result,)
 
@@ -96,11 +99,12 @@ class J_StringSelect:
     def INPUT_TYPES(s):
         return {
             "required": {
-                # "input": ("LIST", {"forceInput": True}),
-                "input": ("STRING", { "multiline": False, "default": "", "forceInput": True }),
+                "input_list": ("STRING", { "forceInput": True }),
                 "select": ("INT", { "default": 0, "min": 0, "step": 1, "display": "number" }),
             },
         }
+    
+    INPUT_IS_LIST = True
 
     RETURN_TYPES = ("STRING",)
     RETURN_NAMES = ("selected",)
@@ -111,16 +115,59 @@ class J_StringSelect:
 
     CATEGORY = "JNodes"
 
-    def string_select(self, input_list, select_index):
-        print(f"input_list: {input_list}", sep="\n")
-        result = input_list[select_index]
-        return (result,)
+    def string_select(self, input_list, select):
+        selectIndex = select[0]
+        if len(input_list) > 0 and selectIndex < len(input_list):
+            result = input_list[selectIndex]
+            return (result,)
+        
+        return ("",)
 
+OUTPUT_TYPE_MATCHED_STRING = "matched string"
+OUTPUT_TYPE_MATCHED_GENDER = "matched gender"
+
+class J_StringSelectByGender:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "input_list": ("STRING", { "forceInput": True }),
+                "default_if_not_found": ("STRING", { "multiline": False, "default": "person" }),
+                "output_type": ([OUTPUT_TYPE_MATCHED_STRING, OUTPUT_TYPE_MATCHED_GENDER],),
+            },
+        }
+    
+    INPUT_IS_LIST = True
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("match",)
+
+    FUNCTION = "string_select"
+
+    OUTPUT_NODE = True
+
+    CATEGORY = "JNodes"
+
+    def string_select(self, input_list, default_if_not_found, output_type):
+        default = default_if_not_found[0]
+        if len(input_list) == 0:
+            return (default,)
+        
+        rx = re.compile(r"\b(man|woman|girl|boy)\b")
+        for s in input_list:
+            match = rx.search(s)
+            if match:
+                if (output_type[0] == OUTPUT_TYPE_MATCHED_GENDER):
+                    return (match[0],)
+                return (s,)
+        
+        return (default,)
 
 
 NODE_CLASS_MAPPINGS = {
-    "J_StringContains": J_StringContains,
-    "J_StringAppend": J_StringAppend,
-    "J_StringSplit": J_StringSplit,
-    "J_StringSelect": J_StringSelect,
+    "J Contains String": J_StringContains,
+    "J Append String": J_StringAppend,
+    "J Split String": J_StringSplit,
+    "J Select String": J_StringSelect,
+    "J Select String By Gender": J_StringSelectByGender,
 }
